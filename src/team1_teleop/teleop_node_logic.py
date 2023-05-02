dfedbc77e6a34ea43a8bd0a7fa62e54bd4771b33
James Froelich <jamesfroelich@Jamess-MacBook-Pro-56.local>
2023-05-02 13:30:06 -0700

    node update

--- src/team1_teleop/teleop_node_logic.py
+++ src/team1_teleop/teleop_node_logic.py
@@ -11,24 +11,44 @@ class TeleopNode:
     def __init__(self):
         self.robot = stretch_body.robot.Robot()
         self.robot.startup()
-        self.arm_pos = 0.0
+        #self.arm_pos = 0.0
         self.robot.arm.move_to(0.0)
         self.robot.push_command()
         self.robot.arm.wait_until_at_setpoint()
         self.pub = rospy.Publisher('arm_pos', Float64, queue_size=10)
-        self.sub = rospy.Subscriber('arm_move_commands', Float64, self.move_callback)
+        self.sub = rospy.Subscriber('arm_move_commands', Float64, self.move_arm_callback)
+        self.sub = rospy.Subscriber('body_move_commands', Float64, self.body_arm_callback)
         self.timer = rospy.Timer(rospy.Duration(0.5), self.timer_callback)
         
 
-    def move_callback(self, data):
+    def move_arm_callback(self, data):
         rospy.loginfo(rospy.get_caller_id() + "Arm move command received %f", data.data)
         # self.robot.base.translate_by(x_m=data.data)
-        self.arm_pos += data.data
-        if self.arm_pos < 0:
-            self.arm_pos = 0
-        self.robot.arm.move_to(self.arm_pos)
+
+        arm_min = 0.0
+        arm_max = 0.51
+        T = 0.00001
+
+        cur_pos = self.robot.arm.status['pos']
+        new_pos = cur_pos + data.data
+        if new_pos < arm_min:
+            new_pos = arm_min
+        elif new_pos > arm_max:
+            new_pos = arm_max
+        if (not (abs(new_pos-cur_pos) < T)):
+          self.robot.arm.move_to(new_pos)
+          self.robot.push_command()
+          self.robot.arm.wait_until_at_setpoint()
+
+    def move_body_callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + "body move command received %f", data.data)
+        # self.robot.base.translate_by(x_m=data.data)
+        body_pos = self.robot.body.status['pos'] + data.data
+        # if self.arm_pos < 0:
+        #     self.arm_pos = 0
+        self.robot.body.move_to(body_pos)
         self.robot.push_command()
-        self.robot.arm.wait_until_at_setpoint()
+        self.robot.body.wait_until_at_setpoint()
     
     def timer_callback(self, timer):
         pos = self.robot.arm.status['pos']
