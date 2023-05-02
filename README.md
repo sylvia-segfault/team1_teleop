e588c2a08d3d6a6dc2deb80f0180e0a455ff2ad5
James Froelich <jamesfroelich@Jamess-MacBook-Pro-56.local>
2023-05-02 11:27:27 -0700

    updates

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Terminal 1:
 cd ~/catkin_ws/
 catkin build
 source devel/setup.bash
+stretch_robot_home.py
 roslaunch team1_teleop websocket.launch
 ```
 
--- run_ui.sh
+++ run_ui.sh
@@ -2,6 +2,7 @@
 cd ~/catkin_ws/
 catkin build
 source devel/setup.bash
+stretch_robot_home.py
 roslaunch team1_teleop websocket.launch & \
 rosrun team1_teleop teleop_node & \
 python3 -m http.server
