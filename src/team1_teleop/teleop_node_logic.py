63b4255b62a4b386fd9729afc2d1b149d42fbc63
Sylvia Wang <sylviawang2017@gmail.com>
2023-05-18 17:15:26 -0700

    using funmap for 2d navigation

--- src/team1_teleop/teleop_node_logic.py
+++ src/team1_teleop/teleop_node_logic.py
@@ -26,14 +26,6 @@ class TeleopNode(hm.HelloNode):
         
     def main(self, node_name, node_topic_namespace, wait_for_first_pointcloud=False):
         super().main(node_name, node_topic_namespace, wait_for_first_pointcloud)
-        # self.robot = stretch_body.robot.Robot()
-        # self.gripper = stretch_body.stretch_gripper.StretchGripper()
-        self.robot.startup()
-        self.head = self.robot.head
-        self.lift = self.robot.lift
-        self.arm = self.robot.arm
-        self.end_of_arm = self.robot.end_of_arm
-        self.base = self.robot.base
 
         # string pose_name -> (string frame_id, Pose robot_2d_loc, Pose walker_2d_loc)
         self.saved_poses_file = rospy.get_param("~/saved_poses", "/home/team1/catkin_ws/src/team1_teleop/saved_poses/saved_poses.pkl")
@@ -53,8 +45,6 @@ class TeleopNode(hm.HelloNode):
         # walker 2d pose
         self.walker_center = None
         self.curr_pos = None
-        self.tf2_buffer = tf2_ros.Buffer()
-        # tf2_ros.TransformListener(self.tf2_buffer)
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         # keep track of user's input (when they want to navigate the walker)
         self.pose_in_coordframe = rospy.Subscriber('pose_in_cf_cmd', String, self.pose_in_coordframe_callback)
@@ -85,71 +75,73 @@ class TeleopNode(hm.HelloNode):
         *** Simple joint subscribers and publishers ***
         ***********************************************
         """
-        self.pub_head_pan = rospy.Publisher('head_pan_pos', Float64, queue_size=10)
-        self.pub_head_tilt = rospy.Publisher('head_tilt_pos', Float64, queue_size=10)
-        self.pub_lift = rospy.Publisher('lift_pos', Float64, queue_size=10)
-        self.pub_arm = rospy.Publisher('arm_pos', Float64, queue_size=10)
-        self.pub_grip = rospy.Publisher('grip_pos', Float64, queue_size=10)
-        self.pub_wrist = rospy.Publisher('wrist_pos', Float64, queue_size=10)
+        # self.pub_head_pan = rospy.Publisher('head_pan_pos', Float64, queue_size=10)
+        # self.pub_head_tilt = rospy.Publisher('head_tilt_pos', Float64, queue_size=10)
+        # self.pub_lift = rospy.Publisher('lift_pos', Float64, queue_size=10)
+        # self.pub_arm = rospy.Publisher('arm_pos', Float64, queue_size=10)
+        # self.pub_grip = rospy.Publisher('grip_pos', Float64, queue_size=10)
+        # self.pub_wrist = rospy.Publisher('wrist_pos', Float64, queue_size=10)
         
         self.sub_head_pan = rospy.Subscriber('head_pan_cmd', Float64, self.move_head_pan_callback)
         self.sub_head_tilt = rospy.Subscriber('head_tilt_cmd', Float64, self.move_head_tilt_callback)
-        self.sub_lift = rospy.Subscriber('lift_cmd', Float64, self.move_lift_callback)
-        self.sub_arm = rospy.Subscriber('arm_cmd', Float64, self.move_arm_callback)
-        self.sub_grip = rospy.Subscriber('grip_cmd', Float64, self.move_grip_callback)
-        self.sub_wrist = rospy.Subscriber('wrist_cmd', Float64, self.move_wrist_callback)
-        self.sub_pose = rospy.Subscriber('pose_cmd', FrontEnd, self.move_pose_callback)
+        # self.sub_lift = rospy.Subscriber('lift_cmd', Float64, self.move_lift_callback)
+        # self.sub_arm = rospy.Subscriber('arm_cmd', Float64, self.move_arm_callback)
+        # self.sub_grip = rospy.Subscriber('grip_cmd', Float64, self.move_grip_callback)
+        # self.sub_wrist = rospy.Subscriber('wrist_cmd', Float64, self.move_wrist_callback)
+        # self.sub_pose = rospy.Subscriber('pose_cmd', FrontEnd, self.move_pose_callback)
 
-        self.sub_translate_base = rospy.Subscriber('translate_base_cmd', Float64, self.translate_base_callback, queue_size=1)
-        self.sub_rotate_base = rospy.Subscriber('rotate_base_cmd', Float64, self.rotate_base_callback, queue_size=1)
-        self.sub_stop_base = rospy.Subscriber('stop_base_cmd', Float64, self.stop_base_callback)
+        # self.sub_translate_base = rospy.Subscriber('translate_base_cmd', Float64, self.translate_base_callback, queue_size=1)
+        # self.sub_rotate_base = rospy.Subscriber('rotate_base_cmd', Float64, self.rotate_base_callback, queue_size=1)
+        # self.sub_stop_base = rospy.Subscriber('stop_base_cmd', Float64, self.stop_base_callback)
 
-        self.set_motion_limits()
+        # self.set_motion_limits()
         rospy.loginfo("Node initialized")
     
-    def set_motion_limits(self):
-        self.lift.set_soft_motion_limit_min(0.25)
-        self.lift.set_soft_motion_limit_max(1)
+    # def set_motion_limits(self):
+    #     self.lift.set_soft_motion_limit_min(0.25)
+    #     self.lift.set_soft_motion_limit_max(1)
 
-        self.arm.set_soft_motion_limit_min(0.0)
-        self.arm.set_soft_motion_limit_max(1)
+    #     self.arm.set_soft_motion_limit_min(0.0)
+    #     self.arm.set_soft_motion_limit_max(1)
     
     
     def save_pose_callback(self, msg: SavePose):
-        if self.curr_pos is None or self.walker_center is None:
-            # TODO: This should be a service which can indicate that it failed
-            rospy.logwarn("save pose callback returned early: " 
-                          + self.curr_pos + ", " + self.walker_center)
-            return
+        # if self.curr_pos is None or self.walker_center is None:
+        #     # TODO: This should be a service which can indicate that it failed
+        #     rospy.logwarn("save pose callback returned early: " 
+        #                   + self.curr_pos + ", " + self.walker_center)
+        #     return
         try:
             # self.walker_center is the walker pose in the 2d frame of the map
             # Note that source frame in lookup_transform 'walker_center' should 
             # instead be whatever frame self.walker_center is saved in ("map")
-            walker_trans = self.tf2_buffer.lookup_transform(msg.pose_frame_id, 'walker_center', self.walker_pos_3d.header.stamp, rospy.Duration(1))
+            # walker_trans = self.tf2_buffer.lookup_transform(msg.pose_frame_id, 'walker_center', self.walker_pos_3d.header.stamp, rospy.Duration(1))
             odom_trans = self.tf2_buffer.lookup_transform(msg.pose_frame_id, 'odom', self.walker_pos_3d.header.stamp, rospy.Duration(1))
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            print(e)
             rospy.logwarn("Cannot transform saved pose")
             return
         rospy.loginfo("saved pose: " + msg.pose_name)
-        walker_pos = tf2_geometry_msgs.do_transform_pose(self.walker_center, walker_trans)
+        # walker_pos = tf2_geometry_msgs.do_transform_pose(self.walker_center, walker_trans)
         odom_pose = tf2_geometry_msgs.do_transform_pose(self.curr_pos, odom_trans)
-        self.saved_poses[msg.pose_name] = (msg.pose_frame_id, odom_pose, walker_pos)
+        self.saved_poses[msg.pose_name] = (msg.pose_frame_id, odom_pose)
 
     def front_end_timer_callback(self, timer):
-        lift_pos = self.robot.lift.status['pos']
-        arm_pos = self.robot.arm.status['pos']
-        grip_pos = self.robot.end_of_arm.status['stretch_gripper']['pos']
-        wrist_pos = self.robot.end_of_arm.status['wrist_yaw']['pos']
-        head_pan = self.robot.head.status['head_pan']['pos']
-        head_tilt = self.robot.head.status['head_tilt']['pos']
+        # lift_pos = self.robot.lift.status['pos']
+        # arm_pos = self.robot.arm.status['pos']
+        # grip_pos = self.robot.end_of_arm.status['stretch_gripper']['pos']
+        # wrist_pos = self.robot.end_of_arm.status['wrist_yaw']['pos']
+        # head_pan = self.robot.head.status['head_pan']['pos']
+        # head_tilt = self.robot.head.status['head_tilt']['pos']
 
         # rospy.loginfo("Publishing lift {}, arm {}, gripper {}, wrist {}, head pan {}, head tilt {}". format(lift_pos, arm_pos, grip_pos, wrist_pos, head_pan, head_tilt))
-        self.pub_lift.publish(lift_pos)
-        self.pub_arm.publish(arm_pos)
-        self.pub_grip.publish(grip_pos)
-        self.pub_wrist.publish(wrist_pos)
-        self.pub_head_pan.publish(head_pan)
-        self.pub_head_tilt.publish(head_tilt)
+        # self.pub_lift.publish(lift_pos)
+        # self.pub_arm.publish(arm_pos)
+        # self.pub_grip.publish(grip_pos)
+        # self.pub_wrist.publish(wrist_pos)
+        # self.pub_head_pan.publish(head_pan)
+        # self.pub_head_tilt.publish(head_tilt)
+
         self.pub_saved_poses.publish(','.join(self.saved_poses.keys()))
         # rospy.loginfo("sent poses: " + ','.join(self.saved_poses.keys()))
     
@@ -192,7 +184,7 @@ class TeleopNode(hm.HelloNode):
         # self.walker_center = walker_pos_2d
 
     def pose_in_coordframe_callback(self, msg):
-        frame_id, robot_pose, _ = self.saved_poses[msg.data]
+        frame_id, robot_pose = self.saved_poses[msg.data]
         try:
             trans = self.tf2_buffer.lookup_transform("map", frame_id, self.walker_pos_3d.header.stamp, rospy.Duration(1))
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
@@ -227,87 +219,79 @@ class TeleopNode(hm.HelloNode):
 
     def move_head_pan_callback(self, data):
         rospy.loginfo(rospy.get_caller_id() + "Head pan move command received %f", data.data)
-        self.head.move_to('head_pan', data.data)
-        self.robot.push_command()
-        self.head.get_joint('head_pan').wait_until_at_setpoint()
-
-        # pose = {"joint_head_pan": data.data}
-        # self.move_to_pose(pose)
+        pose = {"joint_head_pan": data.data}
+        self.move_to_pose(pose)
 
     def move_head_tilt_callback(self, data):
         rospy.loginfo(rospy.get_caller_id() + "Head tilt move command received %f", data.data)
-        # pose = {"joint_head_tilt": data.data}
-        # self.move_to_pose(pose)
-        self.head.move_to('head_tilt', data.data)
-        self.robot.push_command()
-        self.head.get_joint('head_tilt').wait_until_at_setpoint()    
-
-    def move_lift_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "lift move command received %f", data.data)
-        self.robot.lift.move_to(data.data)
-        self.robot.push_command()
-        self.robot.lift.wait_until_at_setpoint()
-
-    def move_arm_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "Arm move command received %f", data.data)
-        self.robot.arm.move_to(data.data)
-        self.robot.push_command()
-        self.robot.arm.wait_until_at_setpoint()
-
-    def move_wrist_callback(self, data):
-        rospy.logwarn("HERE")
-        rospy.loginfo(rospy.get_caller_id() + "Wrist move command received %f", data.data)
-        # self.end_of_arm.move_to('wrist_yaw', data.data)
-        # self.robot.push_command()
-        # self.end_of_arm.get_joint('wrist_yaw').wait_until_at_setpoint()
-        pose = {"wrist_extension": data.data}
+        pose = {"joint_head_tilt": data.data}
         self.move_to_pose(pose)
+
+    # def move_lift_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "lift move command received %f", data.data)
+    #     self.robot.lift.move_to(data.data)
+    #     self.robot.push_command()
+    #     self.robot.lift.wait_until_at_setpoint()
+
+    # def move_arm_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "Arm move command received %f", data.data)
+    #     self.robot.arm.move_to(data.data)
+    #     self.robot.push_command()
+    #     self.robot.arm.wait_until_at_setpoint()
+
+    # def move_wrist_callback(self, data):
+    #     rospy.logwarn("HERE")
+    #     rospy.loginfo(rospy.get_caller_id() + "Wrist move command received %f", data.data)
+    #     # self.end_of_arm.move_to('wrist_yaw', data.data)
+    #     # self.robot.push_command()
+    #     # self.end_of_arm.get_joint('wrist_yaw').wait_until_at_setpoint()
+    #     pose = {"wrist_extension": data.data}
+    #     self.move_to_pose(pose)
     
-    def move_grip_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "Gripper move command received %f", data.data)
-        pct_unit = self.gripper.world_rad_to_pct(data.data)
-        self.end_of_arm.move_to('stretch_gripper', pct_unit)
-        self.robot.push_command()
-        self.end_of_arm.get_joint('stretch_gripper').wait_until_at_setpoint()
+    # def move_grip_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "Gripper move command received %f", data.data)
+    #     pct_unit = self.gripper.world_rad_to_pct(data.data)
+    #     self.end_of_arm.move_to('stretch_gripper', pct_unit)
+    #     self.robot.push_command()
+    #     self.end_of_arm.get_joint('stretch_gripper').wait_until_at_setpoint()
 
-    def translate_base_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "Base command received %f", data.data)
-        self.base.set_translate_velocity(v_m=data.data)
-        self.robot.push_command()    
+    # def translate_base_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "Base command received %f", data.data)
+    #     self.base.set_translate_velocity(v_m=data.data)
+    #     self.robot.push_command()    
     
-    def rotate_base_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "Rotate command received %f", data.data)
-        self.base.set_rotational_velocity(v_r=data.data)
-        self.robot.push_command()
+    # def rotate_base_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "Rotate command received %f", data.data)
+    #     self.base.set_rotational_velocity(v_r=data.data)
+    #     self.robot.push_command()
 
-    def stop_base_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "stop command received %f", data.data)
-        self.base.set_translate_velocity(v_m=data.data)
-        self.base.set_rotational_velocity(v_r=data.data)
-        self.robot.push_command()
+    # def stop_base_callback(self, data):
+    #     rospy.loginfo(rospy.get_caller_id() + "stop command received %f", data.data)
+    #     self.base.set_translate_velocity(v_m=data.data)
+    #     self.base.set_rotational_velocity(v_r=data.data)
+    #     self.robot.push_command()
 
-    def move_pose_callback(self, msg):
-        """ Given a Front End msg move every joint of the robot """
-        rospy.loginfo(msg)
-        self.robot.lift.move_to(msg.lift)
-        self.robot.arm.move_to(msg.arm)
-        self.end_of_arm.move_to('stretch_gripper', self.gripper.world_rad_to_pct(msg.grip))
-        self.end_of_arm.move_to('wrist_yaw', msg.wrist)
-        self.head.move_to('head_pan', msg.head_pan)
-        self.head.move_to('head_tilt', msg.head_tilt)
+    # def move_pose_callback(self, msg):
+    #     """ Given a Front End msg move every joint of the robot """
+    #     rospy.loginfo(msg)
+    #     self.robot.lift.move_to(msg.lift)
+    #     self.robot.arm.move_to(msg.arm)
+    #     self.end_of_arm.move_to('stretch_gripper', self.gripper.world_rad_to_pct(msg.grip))
+    #     self.end_of_arm.move_to('wrist_yaw', msg.wrist)
+    #     self.head.move_to('head_pan', msg.head_pan)
+    #     self.head.move_to('head_tilt', msg.head_tilt)
         
-        self.robot.push_command()
+    #     self.robot.push_command()
 
-        self.lift.wait_until_at_setpoint()
-        self.arm.wait_until_at_setpoint()
-        self.end_of_arm.get_joint('stretch_gripper').wait_until_at_setpoint()
-        self.end_of_arm.get_joint('wrist_yaw').wait_until_at_setpoint()
-        self.head.get_joint('head_pan').wait_until_at_setpoint()
-        self.head.get_joint('head_tilt').wait_until_at_setpoint()
+    #     self.lift.wait_until_at_setpoint()
+    #     self.arm.wait_until_at_setpoint()
+    #     self.end_of_arm.get_joint('stretch_gripper').wait_until_at_setpoint()
+    #     self.end_of_arm.get_joint('wrist_yaw').wait_until_at_setpoint()
+    #     self.head.get_joint('head_pan').wait_until_at_setpoint()
+    #     self.head.get_joint('head_tilt').wait_until_at_setpoint()
 
     def shutdown_hook(self):
         rospy.loginfo("Shutting down TeleopNode")
-        self.robot.stop()
         if len(self.saved_poses):
             with open(self.saved_poses_file, "wb") as file:
                 pickle.dump(self.saved_poses, file)
